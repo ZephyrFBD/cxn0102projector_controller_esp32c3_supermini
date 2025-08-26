842087107bc56c0a976fd2c6ae1d61488d45b28a
Zephyr <Zephyr142024@gmail.com>
2025-08-27 01:20:47 +0800

    up3.4

--- v3.3/.pio/build/esp32-c3-devkitm-1/FrameworkArduino/FirmwareMSC.cpp.d
+++
@@ -1,5 +0,0 @@
-.pio/build/esp32-c3-devkitm-1/FrameworkArduino/FirmwareMSC.cpp.o: \
- C:/Users/skyve/.platformio/packages/framework-arduinoespressif32/cores/esp32/FirmwareMSC.cpp \
- C:/Users/skyve/.platformio/packages/framework-arduinoespressif32/cores/esp32/FirmwareMSC.h \
- C:/Users/skyve/.platformio/packages/framework-arduinoespressif32/cores/esp32/USBMSC.h \
- C:/Users/skyve/.platformio/packages/framework-arduinoespressif32/tools/sdk/esp32c3/dio_qspi/include/sdkconfig.h
--- v3.3/.pio/build/esp32-c3-devkitm-1/FrameworkArduino/USB.cpp.d
+++
@@ -1,4 +0,0 @@
-.pio/build/esp32-c3-devkitm-1/FrameworkArduino/USB.cpp.o: \
- C:/Users/skyve/.platformio/packages/framework-arduinoespressif32/cores/esp32/USB.cpp \
- C:/Users/skyve/.platformio/packages/framework-arduinoespressif32/cores/esp32/USB.h \
- C:/Users/skyve/.platformio/packages/framework-arduinoespressif32/tools/sdk/esp32c3/dio_qspi/include/sdkconfig.h
